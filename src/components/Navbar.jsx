6980fb72d255b2810bbfefdeef91808def0c7e81
prasiddha <prasiddhadahal33@gmail.com>
2026-03-02 22:04:00 +0545

    feat: added ThemeToggle Component in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react"
+import ThemeToggle from "./ThemeToggle";
 
-const Navbar = () => {
+const Navbar = ({ onSearch }) => {
     const [input, Setinput] = useState("");
 
     const handleChange = (e) => {
         const value = e.target.value;
         Setinput(value);
+        onSearch(value);
     }
 
     return (
-        <nav className="flex justify-between items-center p-6 bg-slate-100 dark:bg-slate-900">
-            <h1 className="text-2xl font-bold">MovieApp</h1>
+        <nav className="flex justify-between items-center p-6 bg-slate-300 dark:bg-slate-900">
+            <h1 className="text-2xl font-bold dark:text-white">MovieApp</h1>
             <input type="text"
                     placeholder="Search Movies..."
                     value={input}
                     onChange={handleChange}
-                    className="rounded-xl px-4 py-2 w-1/3 bg-white text-slate-900 dark:bg-slate-700 dark:text-white"
-                    
+                    className="rounded-xl px-4 py-2 w-1/3 bg-white outline-none text-slate-900 dark:bg-slate-700 dark:text-white"
             />
-            <button className="border">Toggle</button>
+            <ThemeToggle />
         </nav>
     )
 }
---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
+const ThemeToggle = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
+    return (
+        <div
+            onClick={toggleTheme}
+            className=" w-14 h-8 flex items-center bg-slate-500 dark:bg-slate-300 rounded-full p-1 cursor-pointer transition-colors duration-300"
+        >
+            <div
+                className={`bg-white dark:bg-slate-900 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 ${
+                    theme === "dark" ? "translate-x-6" : "translate-x-0"
+                }`}
+            />
+        </div>
+    );
+};
+
+export default ThemeToggle;
